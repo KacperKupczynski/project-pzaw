c1d13a6e979cd469e7dbc75f8453ebadcf420d95
Kacper Kupczyński <kacperkupczynski2017@gmail.com>
2024-12-08 13:49:21 +0100

    rework on project setup

---
+++ frontend/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let users = [];
+
+  const fetchUsers = async () => {
+      const response = await fetch('/api/users/');
+      users = await response.json();
+  };
+
+  fetchUsers();
+</script>
+
+<h1>Lista użytkowników</h1>
+<ul>
+  {#each users as user}
+      <li>{user.name} ({user.email})</li>
+  {/each}
+</ul>
